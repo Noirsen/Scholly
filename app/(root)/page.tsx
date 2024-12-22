b3dc2991a2f9dc6a9ce1aca0a5aa21ab52c53a1c
Noirsen <profzumar@gmail.com>
2024-12-22 09:56:30 +0700

    card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,6 @@
+import StartupCard from "@/components/StartupCard";
+import SearchForm from "../../components/SearchForm";
+
 export default async function Home({
   searchParams,
 }: {
@@ -6,18 +9,18 @@ export default async function Home({
   const query = (await searchParams).query;
   const params = { search: query || null };
 
-  //const posts = [
-  //{
-  // _createAt: new Date(),
-  //views: 55,
-  //author: { _id: 1, name: "MbakSilvana" },
-  //_id: 1,
-  //description: "this is a desctiption",
-  //image: "https://wallpapercave.com/wp/wp3803471.jpg",
-  //category: "Robots",
-  //title: "We Robot",
-  //},
-  //];
+  const posts = [
+    {
+      _createAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "MbakSilvana" },
+      _id: 1,
+      description: "this is a desctiption",
+      image: "https://wallpapercave.com/wp/wp3803471.jpg",
+      category: "Robots",
+      title: "We Robot",
+    },
+  ];
 
   return (
     <>
@@ -30,12 +33,23 @@ export default async function Home({
         <p className="sub-heading !max-w-3xl">
           Temukan Impian, Raih Beasiswa, dan jadi boneka bagi orang berduit.
         </p>
+
+        <SearchForm query={query} />
       </section>
 
       <section className="section_container">
         <p className="text-30-semibold">
           {query ? `Search results for "${query}"` : "All Startups"}
         </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
