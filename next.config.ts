2ad48652dbc6e11abb17870fb2bc8612aeb963ef
Noirsen <profzumar@gmail.com>
2024-12-22 23:40:21 +0700

    kowalski detail

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  /* config options here */
+  experimental: {
+    ppr: "incremental",
+  },
+  devIndicators: {
+    appIsrStatus: true,
+    buildActivity: true,
+    buildActivityPosition: "bottom-right",
+  },
 };
 
 export default nextConfig;
